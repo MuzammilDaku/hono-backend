a2b9a1bb8e3cd4b4dc265df0af9f0d8f71f9901d
MuzammilDaku <khanbalochmuzammilabbas@gmail.com>
2025-08-13 23:07:14 +0500

    Latest Code

---
+++ src/db/index.ts
@@ -0,0 +1,19 @@
+import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
+// @ts-expect-error
+import postgres, { Sql } from "postgres";
+
+let client: Sql | undefined;
+let db: PostgresJsDatabase | undefined;
+
+export function dbConnection() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined in the environment variables");
+  }
+
+  if (!client) {
+    client = postgres(process.env.DATABASE_URL, { ssl: 'require' }); // for production
+    db = drizzle(client!);
+  }
+
+  return db;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import { Hono } from 'hono'
-
+import { dbConnection } from './db'
+import { user } from './db/schemas'
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.post('/api', async (c) => {
+ try {
+   const body = await c.req.json();
+
+  const db = await dbConnection()
+  if (!db) {
+    return c.json({ error: 'Database connection failed' }, 500);
+  }
+  const [response] = await db.insert(user).values(body).returning();
+  console.log(response)
+  return c.json( response, 201)
+ } catch (error) {
+  console.error('Error inserting data:', error);
+  return c.json({ error: error}, 500)
+ }
 })
 
 export default app
